887f0ac2a69e55241e2f29cea19cfd1877d35c4b
prashant kaushik <41025191+hack3rinsider@users.noreply.github.com>
2025-07-01 15:19:35 +0530

    Update tomcat-provision.sh

--- userdata/tomcat-provision.sh
+++ userdata/tomcat-provision.sh
@@ -1,107 +1,76 @@
 #!/bin/bash
+set -e  # Exit immediately on any error
+
+# Update and install core packages
 sudo apt update
-sudo apt install openjdk-8-jdk -y
-sudo apt install git wget unzip -y
-sudo apt install awscli -y
+sudo apt install -y openjdk-8-jdk git wget unzip curl
+
+# Install AWS CLI v2 (since awscli is not in apt on 24.04+)
+cd /tmp
+curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
+sudo ./aws/install || true  # tolerate if already installed
+
+# Create tomcat user if it doesn't exist
+sudo useradd --system --shell /usr/sbin/nologin --home /usr/local/tomcat8 tomcat || true
+
+# Download and extract Tomcat
 TOMURL="https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.37/bin/apache-tomcat-8.5.37.tar.gz"
-cd /tmp/
-wget $TOMURL -O tomcatbin.tar.gz
-EXTOUT=`tar xzvf tomcatbin.tar.gz`
-TOMDIR=`echo $EXTOUT | cut -d '/' -f1`
-useradd --shell /sbin/nologin tomcat
-rsync -avzh /tmp/$TOMDIR/ /usr/local/tomcat8/
-rm -rf /usr/local/tomcat8/conf/tomcat-users.xml
-rm -rf /usr/local/tomcat8/webapps/manager/META-INF/context.xml
-touch /usr/local/tomcat8/webapps/manager/META-INF/context.xml
-touch /usr/local/tomcat8/conf/tomcat-users.xml
-cat <<EOT>> /usr/local/tomcat8/conf/tomcat-users.xml
+cd /tmp
+wget -q "$TOMURL" -O tomcat.tar.gz
+tar -xzf tomcat.tar.gz
+TOMDIR=$(tar -tf tomcat.tar.gz | head -1 | cut -d '/' -f1)
+
+# Move Tomcat to target directory
+sudo mkdir -p /usr/local/tomcat8
+sudo rsync -a "/tmp/$TOMDIR/" /usr/local/tomcat8/
+
+# Configure tomcat-users.xml
+sudo tee /usr/local/tomcat8/conf/tomcat-users.xml > /dev/null <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
-<!--
-  Licensed to the Apache Software Foundation (ASF) under one or more
-  contributor license agreements.  See the NOTICE file distributed with
-  this work for additional information regarding copyright ownership.
-  The ASF licenses this file to You under the Apache License, Version 2.0
-  (the "License"); you may not use this file except in compliance with
-  the License.  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
--->
 <tomcat-users xmlns="http://tomcat.apache.org/xml"
-xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-xsi:schemaLocation="http://tomcat.apache.org/xml tomcat-users.xsd"
-version="1.0">
-<!--
-  NOTE:  By default, no user is included in the "manager-gui" role required
-  to operate the "/manager/html" web application.  If you wish to use this app,
-  you must define such a user - the username and password are arbitrary. It is
-  strongly recommended that you do NOT use one of the users in the commented out
-  section below since they are intended for use with the examples web
-  application.
--->
-<!--
-  NOTE:  The sample user and role entries below are intended for use with the
-  examples web application. They are wrapped in a comment and thus are ignored
-  when reading this file. If you wish to configure these users for use with the
-  examples web application, do not forget to remove the <!.. ..> that surrounds
-  them. You will also need to set the passwords to something appropriate.
--->
+              xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+              xsi:schemaLocation="http://tomcat.apache.org/xml tomcat-users.xsd"
+              version="1.0">
   <role rolename="manager-gui"/>
   <role rolename="manager-script"/>
   <user username="tomcat" password="admin123" roles="manager-gui,manager-script"/>
 </tomcat-users>
-EOT
+EOF
 
-cat <<EOT>> /usr/local/tomcat8/webapps/manager/META-INF/context.xml
+# Allow manager access from all IPs
+sudo mkdir -p /usr/local/tomcat8/webapps/manager/META-INF/
+sudo tee /usr/local/tomcat8/webapps/manager/META-INF/context.xml > /dev/null <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
-<!--
-  Licensed to the Apache Software Foundation (ASF) under one or more
-  contributor license agreements.  See the NOTICE file distributed with
-  this work for additional information regarding copyright ownership.
-  The ASF licenses this file to You under the Apache License, Version 2.0
-  (the "License"); you may not use this file except in compliance with
-  the License.  You may obtain a copy of the License at
+<Context antiResourceLocking="false" privileged="true" />
+EOF
 
-      http://www.apache.org/licenses/LICENSE-2.0
+# Set ownership
+sudo chown -R tomcat:tomcat /usr/local/tomcat8
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
--->
-<Context antiResourceLocking="false" privileged="true" >
-<!--
-  <Valve className="org.apache.catalina.valves.RemoteAddrValve"
-         allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1" />
-  <Manager sessionAttributeValueClassNameFilter="java\.lang\.(?:Boolean|Integer|Long|Number|String)|org\.apache\.catalina\.filters\.CsrfPreventionFilter\$LruCache(?:\$1)?|java\.util\.(?:Linked)?HashMap"/>
--->
-</Context>
-EOT
-chown -R tomcat.tomcat /usr/local/tomcat8 
-cat <<EOT>> /etc/systemd/system/tomcat.service
+# Create systemd service
+sudo tee /etc/systemd/system/tomcat.service > /dev/null <<EOF
 [Unit]
-Description=Tomcat
+Description=Apache Tomcat Web Application Container
 After=network.target
 
 [Service]
+Type=simple
 User=tomcat
-WorkingDirectory=/usr/local/tomcat8
+Group=tomcat
 Environment=CATALINA_HOME=/usr/local/tomcat8
-Environment=CATALINE_BASE=/usr/local/tomcat8
+Environment=CATALINA_BASE=/usr/local/tomcat8
 ExecStart=/usr/local/tomcat8/bin/catalina.sh run
 ExecStop=/usr/local/tomcat8/bin/shutdown.sh
-SyslogIdentifier=tomcat-%i
+Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
-EOT
+EOF
 
-systemctl daemon-reload
-systemctl start tomcat
-systemctl enable tomcat
+# Reload systemd and start Tomcat
+sudo systemctl daemon-reload
+sudo systemctl enable --now tomcat
+
+# Optional: verify port 8080 is open (can be logged in cloud-init)
+curl -s -o /dev/null -w "%{http_code}\n" http://localhost:8080 || true
